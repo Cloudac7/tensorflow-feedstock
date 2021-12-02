ecf3e418112cb2492e18f6513ab79f60fd8b7538
Cloudac7 <812556867@qq.com>
2021-12-02 20:13:07 +0800

    add support for cuda9.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -145,6 +145,8 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
         export TF_CUDA_COMPUTE_CAPABILITIES=sm_35,sm_50,sm_60,sm_62,sm_70,sm_72,sm_75,sm_80,sm_86,compute_86
     elif [[ ${cuda_compiler_version} == 11.3 ]]; then
         export TF_CUDA_COMPUTE_CAPABILITIES=sm_35,sm_50,sm_60,sm_62,sm_70,sm_72,sm_75,sm_80,sm_86,compute_86
+    elif [[ ${cuda_compiler_version} == 9.2 ]]; then
+	export TF_CUDA_COMPUTE_CAPABILITIES=sm_35,sm_50,sm_60,sm_62,sm_70,compute_70
     else
         echo "unsupported cuda version."
         exit 1
